213b403ffa6ea47e0a49440fae335a744df5b889
Salman Shaik <salmansiddiq.shaik@gmail.com>
2026-03-04 04:54:08 +0000

    Added tests to auth package

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"testing"
+)
+
+func TestGetApiKey(t *testing.T) {
+	testHeader := make(map[string][]string)
+
+	testHeader["Authorization"] = []string{"ApiKey TestApiKey12"}
+
+	Want := "TestApiKey"
+	got, err := GetAPIKey(testHeader)
+
+	if err != nil {
+		t.Errorf("Expected: %v, got: %v", Want, got)
+	}
+
+}
